a7d07ba2920b41b68d02936654c5da06f9dd1a3f
Zura Guerra <guerrazuramx@gmail.com>
2016-07-25 20:46:01 -0500

    WIP / Add responsive navbar without breaking everything

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 $(document).foundation();
+
+$(document).ready(function(){
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+   if ($(window).width() >= 1019){
+       if (startchange.length){
+           $(document).scroll(function() {
+               scroll_start = $(this).scrollTop();
+               if(scroll_start > offset.top) {
+                   $(".top-bar").css('background-color', 'rgba(55, 42, 69, 0.9)');
+                   $(".top-bar a").css('color', '#F0F0F0');
+
+               } else {
+                   $('.top-bar').css('background-color', 'transparent');
+                   $(".top-bar a").css('color', '#FFF');
+
+               }
+           });
+
+       }
+   } else {
+       $('.top-bar').css('background-color', 'rgba(55, 42, 69, 0.9)');
+   }
+   if ($(window).width() <= 640){
+       $('.top-bar ul').removeClass('text-center');
+
+   } else {
+       $('.top-bar ul').addClass('text-center');
+   }
+});
