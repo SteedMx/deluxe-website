7b1c4208cb9e77046ab2057f5f011126cb6a2338
Michell Ayala <starships@outlook.com>
2016-08-24 16:47:47 -0500

    change gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,20 @@
+'use strict';
+
 var gulp = require('gulp');
-var $    = require('gulp-load-plugins')();
+var webserver = require('gulp-webserver');
 
-var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
-];
-
-gulp.task('sass', function() {
-  return gulp.src('scss/app.scss')
-    .pipe($.sass({
-      includePaths: sassPaths,
-      outputStyle: 'compressed' // if css compressed **file size**
-    })
-      .on('error', $.sass.logError))
-    .pipe($.autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9']
-    }))
-    .pipe(gulp.dest('css'));
+gulp.task('server', function () {
+  gulp
+    .src('.')
+    .pipe(webserver({
+      host: '0.0.0.0',
+      port: 8080,
+      livereload: true,
+      directoryListing: {
+        enabled: true,
+        path: 'build'
+      }
+    }));
 });
 
-gulp.task('default', ['sass'], function() {
-  gulp.watch(['scss/**/*.scss'], ['sass']);
-});
+gulp.task('default', ['server']);
