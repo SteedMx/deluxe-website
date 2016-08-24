11f9bd0bfbb9088eda78ff4d6b45811c12d23336
Michell Ayala <starships@outlook.com>
2016-08-24 17:21:28 -0500

    change build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,11 @@
 
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var stylus = require('gulp-stylus');
 
 gulp.task('server', function () {
   gulp
-    .src('.')
+    .src('./build')
     .pipe(webserver({
       host: '0.0.0.0',
       port: 8080,
@@ -17,4 +18,22 @@ gulp.task('server', function () {
     }));
 });
 
-gulp.task('default', ['server']);
+gulp.task('watch', function () {
+  gulp.watch('src/styles/**/*.styl', ['css']);
+  gulp.watch('src/index.html', ['html']);
+});
+
+gulp.task('css', function () {
+  gulp
+    .src('src/styles/main.styl')
+    .pipe(stylus({ compress: false }))
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('html', function () {
+  gulp
+    .src('src/index.html')
+    .pipe(gulp.dest('build'));
+});
+
+gulp.task('default', ['server', 'css', 'html', 'watch']);
